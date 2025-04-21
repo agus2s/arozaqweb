f2c37b28b090d383890a8be90aefc2ebf6daa25d
Agus Supriyadi <ayahnya.fiqa@gmail.com>
2025-04-21 20:50:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 
     async function renderMermaid() {
         const elementId = "mermaidChart";
-        const diagramDefinition = `flowchart LR [Mbah Abdurrozaq] --> [Bapak H. Amin]; [Mbah Abdurrozaq] --> [Ibu Mariyah]; [Mbah Abdurrozaq] --> [Ibu Sujiah/Marjiah]; [Mbah Abdurrozaq] --> [Bapak Ilyas]; [Mbah Abdurrozaq] --> [Bapak Jamil]; [Mbah Abdurrozaq] --> [Ibu Satibah]`;
+        const diagramDefinition = `flowchart LR; [Mbah Abdurrozaq] --> [Bapak H. Amin]; [Mbah Abdurrozaq] --> [Ibu Mariyah]; [Mbah Abdurrozaq] --> [Ibu Sujiah/Marjiah]; [Mbah Abdurrozaq] --> [Bapak Ilyas]; [Mbah Abdurrozaq] --> [Bapak Jamil]; [Mbah Abdurrozaq] --> [Ibu Satibah]`;
         const { svg } = await mermaid.render(elementId, diagramDefinition);
         document.getElementById("output").innerHTML = svg;
     }
