634b871b45b8fc44c1dadbc6029b46da957e149d
Agus Supriyadi <ayahnya.fiqa@gmail.com>
2025-04-23 23:39:06 +0700

    Update bani-ahmad-yusuf.php

--- bani-haji-amin/bani-ahmad-yusuf.php
+++ bani-haji-amin/bani-ahmad-yusuf.php
@@ -23,7 +23,7 @@ include 'header.php';
 	</section>
 	<section>
 		<h3>Garis Keturunan</h3>
-		<div class="tree" id="silsilah">
+		<div class="tree">
 			<div class="node"><a href="../index.php">Bapak K.H. Abdurrozaq</div>
 			<div class="line"></div>
 			<div class="node"><a href="index.php">Bapak H. Amin</a></div>
@@ -31,5 +31,37 @@ include 'header.php';
 			<div class="node"><?=NAMA?></div>
 		</div>
 	</section>
+	<section id="silsilah">
+		<h3>Silsilah Keluarga</h3>
+		<div id="output"></div>	
+	</section>
 </main>
-<?php include '../footer.php'; ?>
+<?php 
+const DIAGRAM = 
+	'flowchart LR;
+		A["Bapak H. Amin"] --> B["Bapak Ahmad Yusuf"];
+		B --> C["Bapak/Ibu Lapi"];
+		B --> D["Ibu Sariyah"];
+		B --> E["Bapak Sarman"];
+		B --> F["Ibu Sarmiyah"];
+		B --> G["Ibu Sarilah"];
+		B --> H["Bapak Salimun"];
+		B --> I["Bapak Samingun"];
+		B --> J["Ibu Ludiyah"];
+		B --> K["Bapak Surat"];
+		B --> L["Ibu Ngantun"];
+		B --> M["Ibu Ngaenah"];
+		click A "index.php";
+		click C "bani-lapi.php";
+		click D "bani-sariyah.php";
+		click E "bani-sarman.php";
+		click F "bani-sarmiyah.php";
+		click G "bani-sarilah.php";
+		click H "bani-salimun.php";
+		click I "bani-samingun.php";
+		click J "bani-ludiyah.php";
+		click K "bani-surat.php";
+		click L "bani-ngantun.php";
+	';
+include '../footer.php'; 
+?>
